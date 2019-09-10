093807d978aa63521840ab923935ef97bbdd2d93
a.denysenko <a.denysenko@RDS1.dvsts.com>
2019-09-10 23:55:46 +0300

    added roles dropdown to roles popup

--- src/app/modalwindows/business/role.input.popup.component.ts
+++ src/app/modalwindows/business/role.input.popup.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, ComponentFactory,ComponentRef, ComponentFactoryResolver, ViewContainerRef, ViewChild, Output, EventEmitter} from '@angular/core'
+import {Component, Input, ComponentFactory,ComponentRef, ComponentFactoryResolver, ViewContainerRef, ViewChild, Output, EventEmitter, OnInit} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser';
 import { FormBuilder, FormGroup, FormControl, Validators }  from '@angular/forms';
 import { DynamicDropboxComponent } from './dynamic/dynamic.dropbox.component';
@@ -9,21 +9,43 @@ import { Model } from "../../model/repository.model";
   templateUrl: './role.input.popup.component.html',
   styleUrls: ['./role.input.popup.component.css']
 })
-export class RoleInputPopupComponent {
+export class RoleInputPopupComponent implements OnInit {
     display='none'; //default Variable
     title = 'app';
     componentRef: ComponentRef<any>;
     role: object;
-    application: Array<string> = [];
+    appSelectedDropdownItems = [];
+    applicationDropdownList: Array<string> = [];
 
     @ViewChild('dropboxcontainer', { read: ViewContainerRef }) container;
     constructor(private resolver: ComponentFactoryResolver, private model: Model) {
+        this.getRoleData();
+        console.log("111"+this.applicationDropdownList);
+    }
+    
+    ngOnInit() {        
+        this.dropdownSettings = {
+            singleSelection: true,
+            idField: 'item_id',
+            textField: 'item_text',
+            enableCheckAll: false,
+            itemsShowLimit: 9,
+            allowSearchFilter: false
+        };
         
+        this.dropdownMultiSettings = {
+            singleSelection: false,
+            idField: 'item_id',
+            textField: 'item_text',
+            enableCheckAll: false,
+            itemsShowLimit: 9,
+            allowSearchFilter: false
+        };
     }
     
     openModalDialog(){console.log(this.container);
         this.display='block'; //Set block css
-        this.getRoleData();
+        //this.getRoleData();
         //this.createComponent("Jesus");
         //this.createComponent("Ressurect");
     }
@@ -45,20 +67,26 @@ export class RoleInputPopupComponent {
     
     getRoleData() {
         this.model.getRole().subscribe(data => {
-			if ((data != undefined)) {
-                            this.role = data;
+            let applications: Array<string> = [];
+            if ((data != undefined)) {
+                this.role = data;
                             //Object.values(this.role).forEach(e => console.log(Object.values(e)[0]));
                             //Object.keys(this.role).forEach(e => console.log(Object.keys(e)[0]));
                             
-                            Object.entries(this.role).forEach(entry => this.application.push(Object.values(entry)[0]));
-                            console.log(this.application);
-                            Object.entries(this.role['EMEA EUA & SPO']).forEach(entry => console.log(entry[0]));
-                          console.log(Object.entries(this.role['EMEA EUA & SPO']['ACCESS']));
+                Object.entries(this.role).forEach(entry => applications.push(Object.values(entry)[0]));
                             
+                Object.entries(this.role['EMEA EUA & SPO']).forEach(entry => console.log(entry[0]));
+                console.log(Object.entries(this.role['EMEA EUA & SPO']['ACCESS']));
+                this.applicationDropdownList = applications;
+                           
 //Object.values(this.role).forEach(e => Object.values(e).forEach(el => console.log(el)));
 
                             //this.setUpDropdowns();
 			}
                     });
+    }
+    
+    onApplicationSelect(item: any) {console.log(item.item_text);
+        
     }
 }
